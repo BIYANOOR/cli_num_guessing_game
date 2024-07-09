23225708b7c080636b02638d2b2a2e55a0eaac1a
BIYANOOR <160410736+BIYANOOR@users.noreply.github.com>
2024-07-09 18:18:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+while (true) {
+    const randomNumber = Math.floor(Math.random() * 10);
+    const answers = await inquirer.prompt([
+        {
+            name: "userGuedsNumber",
+            type: "number",
+            message: "Are you ready to play ! please guess a number of your choice"
+        }
+    ]);
+    if (answers.userGuedsNumber === randomNumber) {
+        console.log("🎇🎇Congratulation you guessed a right number🎇🎇");
+    }
+    else {
+        console.log("😞😞  OH You Failed to guess the right number😞😞  best of luck try again😞😞");
+    }
+    ;
+    console.log("LETS PLAY AGAIN");
+}
